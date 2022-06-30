d217e1e30391bce76a7ddb25970cf0d1eb28da4b
EBarmentlo <93001558+EBarmentlo@users.noreply.github.com>
2022-06-30 10:10:13 +0200

    fixed audio part

--- src/App.js
+++ src/App.js
@@ -9,7 +9,15 @@ function App() {
         <main>
           <Dictionary defaultKeyword="sunset" />
         </main>
-        <footer className="app-footer">Coded by Elisabeth Barmentlo</footer>
+        <footer className="app-footer">
+          Coded by Elisabeth Barmentloand it is{" "}
+          <a
+            href="https://github.com/EBarmentlo/react-dictionary-app"
+            target="-blank"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
       </div>
     </div>
   );
